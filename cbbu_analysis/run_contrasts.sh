dd1367d844f7b498c42f78e291fa1cdb2993a34a
ThomasMurray14 <41687196+ThomasMurray14@users.noreply.github.com>
2026-04-24 13:47:15 +0100

    Added contrasts script

---
+++ cbbu_analysis/run_contrasts.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH -D /home/tom29/rds/rds-pal_lab-WJZDLUY2Dhw/CBBU_imaging_analysis/cbbu_analysis
+#SBATCH -A LAWSON-SL3-CPU
+#SBATCH -c 1
+#SBATCH -a 1-44
+#SBATCH -p cclake
+#SBATCH -t 30:00
+#SBATCH --mem 32G
+#SBATCH -o run_contrasts_log/run_contrasts_%A_%a.out
+
+
+# script to run contrasts on first level GLM results
+
+# Load SPM (and matlab)
+module load spm/spm12
+
+# Specify BIDS root directory
+BIDS_root="/home/tom29/rds/rds-pal_lab-WJZDLUY2Dhw/cbbu_BIDS"
+
+# Get subject ID for this job (e.g. "sub-01")
+sub_id=$(printf "sub-%02d" $SLURM_ARRAY_TASK_ID)
+
+# Specify GLM Root directory for this subject
+glm_root="${BIDS_root}/${sub_id}/GLM/No_parametric_modulators"
+
+# Specify output batch file name
+output_batch_fname="${BIDS_root}/${sub_id}/GLM/No_parametric_modulators/first_level_contrasts_batch.mat"
+
+# Print some info for logging
+echo "Running contrasts for ${sub_id}"
+echo "GLM root directory: ${glm_root}"
+echo "Output batch file: ${output_batch_fname}"
+
+# Run matlab function to specify and run contrasts
+matlab -nodisplay -r "addpath('/home/tom29/rds/rds-pal_lab-WJZDLUY2Dhw/cbbu_analysis'); specify_first_level_contrasts('${glm_root}', '${output_batch_fname}'); exit;"
--- cbbu_analysis/specify_first_level_contrasts.m
+++ cbbu_analysis/specify_first_level_contrasts.m
@@ -1,22 +1,59 @@
-
-
-GLM_root = 'C:\Users\Tom\Desktop\cbbu_GLM\sub-20\GLM';
-
-% Specify GLM name
-glm_name = 'No_parametric_modulators';
-
+function specify_first_level_contrasts(GLM_root, output_batch_fname)
+% function to specify and run first level contrasts. Need to specify
+% vectors within this function
+% 
+% GLM_root = 'C:\Users\Tom\Desktop\cbbu_GLM\sub-20\GLM';
+% glm_name = 'No_parametric_modulators';
 
 
 % get SPM.mat
-SPM = load([GLM_root, '\', glm_name, '\SPM.mat']);
+spmmat = [GLM_root, '\SPM.mat'];
+SPM = load(spmmat);
 SPM = SPM.SPM;
 
 % regressor names
 names = SPM.xX.name;
 
+% Specify spm.mat
+matlabbatch{1}.spm.stats.con.spmmat = {spmmat};
+
 % faces > houses
 c = zeros(size(names));
 c(contains(names,'faces*bf(1)')) = 1;
 c(contains(names,'houses*bf(1)')) = -1;
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.name = 'faces>houses';
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.weights = c;
+matlabbatch{1}.spm.stats.con.consess{1}.tcon.sessrep = 'none';
 
-spm_contrasts(SPM, c);
+% houses>faces
+c = zeros(size(names));
+c(contains(names,'faces*bf(1)')) = -1;
+c(contains(names,'houses*bf(1)')) = 1;
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.name = 'houses>faces';
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.weights = c;
+matlabbatch{1}.spm.stats.con.consess{2}.tcon.sessrep = 'none';
+
+% faces>baseline
+c = zeros(size(names));
+c(contains(names,'faces*bf(1)')) = 1;
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.name = 'faces>baseline';
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.weights = c;
+matlabbatch{1}.spm.stats.con.consess{3}.tcon.sessrep = 'none';
+
+% houses>baseline
+c = zeros(size(names));
+c(contains(names,'houses*bf(1)')) = 1;
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.name = 'houses>baseline';
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.weights = c;
+matlabbatch{1}.spm.stats.con.consess{4}.tcon.sessrep = 'none';
+
+% save batch
+save(output_batch_fname, 'matlabbatch');
+
+% run
+if runjob
+    spm('Defaults','fMRI');
+    spm_jobman('initcfg');
+    spm_jobman('run', matlabbatch);
+end
+
